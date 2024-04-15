2b11bd8037e08de19d3a920714042dd72edf6622
mafenzhuangzaimatongli <165651176+mafenzhuangzaimatongli@users.noreply.github.com>
2024-04-15 15:26:10 +0800

    Create WorkingNote.java
    李贵初步添加编辑笔记时添加图片的功能

---
+++ app/src/main/java/net/micode/notes/model/WorkingNote.java
@@ -0,0 +1,8 @@
+// Note Id
+    private long mNoteId;
+    // Note content
+    private String mContent;
+//    private String mContent; //修改0304
+    public String mContent;
+    // Note mode
+    private int mMode;
